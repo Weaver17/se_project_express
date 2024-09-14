687caaeb234f9db8d4df345b34dc8af6d7ba94b2
Andrew <ajweaver18@gmail.com>
2024-09-14 12:05:36 -0400

    user routes

---
+++ models/clothingItem.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "The imageUrl field is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "user",
+    required: true,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+modules.exports = mongoose.model("item", clothingItemSchema);

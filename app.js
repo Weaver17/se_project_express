9a4e4c02c3c06dbe8d88f9943f604a23c8464b92
Andrew <ajweaver18@gmail.com>
2024-09-23 19:42:23 -0400

    fix/ helmet on top

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const helmet = require("helmet");
+
 const cors = require("cors");
 
 const mainRouter = require("./routes/index");
 
 const limiter = require("./utils/rateLimit");
-
-const helmet = require("helmet");
 
 const { PORT = 3001 } = process.env;
 const app = express();

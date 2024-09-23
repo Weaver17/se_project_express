360fd06e5e6e669e1bda6a0af6e04ffa425092ce
Andrew <ajweaver18@gmail.com>
2024-09-23 19:41:19 -0400

    fix/ rate limit

--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -2,7 +2,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 10,
+  limit: 150,
   message: "Limit reached, please try again in 15 min",
 });
 

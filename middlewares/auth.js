c234852a8c59516ae4ef0ee50f6ea7932b52cc29
Andrew <ajweaver18@gmail.com>
2024-11-03 14:42:12 -0500

    fix: 401 error import

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const { JWT_SECRET } = require("../utils/config");
 
-const UnauthorizedError = "../errors/UnauthorizedError";
+const UnauthorizedError = require("../errors/UnauthorizedError");
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;

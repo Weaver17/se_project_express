3fba6750268abd3a0751ffefc93ece2b13c51a0c
Andrew <ajweaver18@gmail.com>
2024-11-03 14:58:13 -0500

    fix: jwt

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,6 @@
-const JWT_SECRET = process.env.JWT_SECRET || "super-strong-secret";
+const { JWT_SECRET = "super-strong-secret" } = process.env;
+
+console.log(JWT_SECRET);
 
 module.exports = {
   JWT_SECRET,
